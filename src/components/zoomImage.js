feda91bdf7c875850929d2b046937459a5b7f222
JL <johnnie-l@Live.ca>
2018-12-28 18:09:04 -0800

    Changed overlay to dark colour, minor issues still

--- src/components/zoomImage.js
+++ src/components/zoomImage.js
@@ -5,6 +5,12 @@ class ZoomImage extends Component {
   render() {
     return (
       <ImageZoom
+        defaultStyles={{ 
+          overlay: { 
+            backgroundColor: '#000000',
+            opacity: '0.6',
+          }
+        }}
         image={{
           src: this.props.src,
           alt: this.props.alt || this.props.caption
