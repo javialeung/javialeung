880debc7cdff8772502128aec3a0433b8e6f93e2
JL <johnnie-l@Live.ca>
2019-01-07 22:48:02 -0800

    Updates

--- path---garments-school-582b2c50ebd5157dd0c7.js
+++ path---garments-school-582b2c50ebd5157dd0c7.js
@@ -1,2 +1,2 @@
-webpackJsonp([3767092402265],{407:function(a,n){a.exports={data:{allMarkdownRemark:null},pathContext:{}}}});
+webpackJsonp([3767092402265],{410:function(a,n){a.exports={data:{allMarkdownRemark:null},pathContext:{}}}});
 //# sourceMappingURL=path---garments-school-582b2c50ebd5157dd0c7.js.map
